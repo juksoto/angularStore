95bc2c60ee75b226cde25901a1330811aa934dcd
Juan C Soto <jsoto@nexura.com>
2019-12-03 14:19:28 -0500

    Data binding en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzi-store';
+  title = 'Store Platzi';
+  nombre = '';
+  apellido = '';
 }
