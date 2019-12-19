ec97727130f0e081031eb0dd4fa426edac8f3e62
Juan Carlos Soto <juksoto@gmail.com>
2019-12-19 15:44:44 -0500

    Listar y usar delete

--- src/app/admin/components/product-list/product-list.component.ts
+++ src/app/admin/components/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ProductsService } from '../../../core/services/products/products.service'
-import { ProductsComponent } from 'src/app/product/components/products/products.component';
+import { Product } from '../../../product/models/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -29,4 +29,42 @@ export class ProductListComponent implements OnInit {
       this.products = products;
     });
   }
+
+  createProduct() {
+    const newProduct: Product = {
+      id: '222',
+      title: 'Nuevo desde angular',
+      image: 'assets/images/banner-1.jpg',
+      price: 3000,
+      description: 'Esta es la descripcion'
+    };
+
+    this.productService.createProduct(newProduct)
+    .subscribe(product => {
+      // products es un object... this products es una array
+      console.log(product);
+      //this.product = product;
+    });
+  }
+    updateProduct() {
+    const updateProduct: Partial<Product> = {
+      title: 'Nuevo Cambio',
+      price: 555,
+    };
+
+    this.productService.updateProduct('2', updateProduct)
+    .subscribe(product => {
+      console.log(product);
+    });
+  }
+  deleteProduct(id: string) {
+    this.productService.deleteProduct(id)
+    // suscribe para ver si se ejecuta
+    .subscribe(product => {
+      //console.log(product);
+      // ejecutamos neuvamente el listado.
+      this.fetchProducts();
+    });
+  }
+
 }
